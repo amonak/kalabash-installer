1f6785a03aee4f379667fd876de90f836c4b43b2
Alphamonak Solutions <amonak@alphamonak.com>
2021-06-15 03:36:47 +0000

    Initial import

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,15 @@
+# Impacted versions
+
+* Distribution: Debian / Ubuntu / Centos
+* Codename: Jessie / Trusty / Centos 7 / ...
+* Arch: 32 Bits / 64 Bits
+* Database: PostgreSQL / MySQL
+
+# Steps to reproduce
+
+# Full trace using --debug option or current behaviour
+
+# Expected behavior
+
+# Video/Screenshot link (optional)
+
---
+++ kalabash_installer/scripts/kalabash.py
@@ -0,0 +1,275 @@
+"""Kalabash related tasks."""
+
+import json
+import os
+import pwd
+import shutil
+import stat
+import sys
+
+from .. import compatibility_matrix
+from .. import package
+from .. import python
+from .. import system
+from .. import utils
+
+from . import base
+
+
+class Kalabash(base.Installer):
+    """Kalabash installation."""
+
+    appname = "kalabash"
+    no_daemon = True
+    packages = {
+        "deb": [
+            "build-essential", "python3-dev", "libxml2-dev", "libxslt-dev",
+            "libjpeg-dev", "librrd-dev", "rrdtool", "libffi-dev", "cron",
+            "libssl-dev", "redis-server", "supervisor"
+        ],
+        "rpm": [
+            "gcc", "gcc-c++", "python3-devel", "libxml2-devel", "libxslt-devel",
+            "libjpeg-turbo-devel", "rrdtool-devel", "rrdtool", "libffi-devel",
+            "supervisor", "redis"
+        ]
+    }
+    config_files = [
+        "crontab=/etc/cron.d/kalabash",
+        "sudoers=/etc/sudoers.d/kalabash",
+    ]
+    with_db = True
+    with_user = True
+
+    def __init__(self, *args, **kwargs):
+        """Get configuration."""
+        super(Kalabash, self).__init__(*args, **kwargs)
+        self.venv_path = self.config.get("kalabash", "venv_path")
+        self.instance_path = self.config.get("kalabash", "instance_path")
+        self.extensions = self.config.get("kalabash", "extensions").split()
+        self.devmode = self.config.getboolean("kalabash", "devmode")
+        # Sanity check for amavis
+        self.amavis_enabled = False
+        if "kalabash-amavis" in self.extensions:
+            if self.config.getboolean("amavis", "enabled"):
+                self.amavis_enabled = True
+            else:
+                self.extensions.remove("kalabash-amavis")
+        if "kalabash-radicale" in self.extensions:
+            if not self.config.getboolean("radicale", "enabled"):
+                self.extensions.remove("kalabash-radicale")
+        self.dovecot_enabled = self.config.getboolean("dovecot", "enabled")
+
+    def is_extension_ok_for_version(self, extension, version):
+        """Check if extension can be installed with this kaldela version."""
+        if extension not in compatibility_matrix.EXTENSIONS_AVAILABILITY:
+            return True
+        version = utils.convert_version_to_int(version)
+        min_version = compatibility_matrix.EXTENSIONS_AVAILABILITY[extension]
+        min_version = utils.convert_version_to_int(min_version)
+        return version >= min_version
+
+    def _setup_venv(self):
+        """Prepare a dedicated virtualenv."""
+        python.setup_virtualenv(
+            self.venv_path, sudo_user=self.user, python_version=3)
+        packages = ["rrdtool"]
+        version = self.config.get("kalabash", "version")
+        if version == "latest":
+            kalabash_package = "kalabash"
+            packages += self.extensions
+        else:
+            matrix = compatibility_matrix.COMPATIBILITY_MATRIX[version]
+            kalabash_package = "kalabash=={}".format(version)
+            for extension in list(self.extensions):
+                if not self.is_extension_ok_for_version(extension, version):
+                    self.extensions.remove(extension)
+                    continue
+                if extension in matrix:
+                    req_version = matrix[extension]
+                    req_version = req_version.replace("<", "\<")
+                    req_version = req_version.replace(">", "\>")
+                    packages.append("{}{}".format(extension, req_version))
+                else:
+                    packages.append(extension)
+        # Temp fix for https://github.com/amonak/kalabash-installer/issues/197
+        python.install_package(
+            kalabash_package, self.venv_path,
+            upgrade=self.upgrade, binary=False, sudo_user=self.user)
+        if self.dbengine == "postgres":
+            packages.append("psycopg2-binary")
+        else:
+            packages.append("mysqlclient")
+        if sys.version_info.major == 2 and sys.version_info.micro < 9:
+            # Add extra packages to fix the SNI issue
+            packages += ["pyOpenSSL"]
+        # Temp fix for https://github.com/amonak/kalabash/issues/2247
+        packages.append("django-webpack-loader==0.7.0")
+        python.install_packages(
+            packages, self.venv_path, upgrade=self.upgrade,
+            sudo_user=self.user
+        )
+        if self.devmode:
+            # FIXME: use dev-requirements instead
+            python.install_packages(
+                ["django-bower", "django-debug-toolbar"], self.venv_path,
+                upgrade=self.upgrade, sudo_user=self.user)
+
+    def _deploy_instance(self):
+        """Deploy Kalabash."""
+        target = os.path.join(self.home_dir, "instance")
+        if os.path.exists(target):
+            condition = (
+                not self.upgrade and
+                not self.config.getboolean("general", "force")
+            )
+            if condition:
+                utils.printcolor(
+                    "Target directory for Kalabash deployment ({}) already "
+                    "exists. If you choose to continue, it will be removed."
+                    .format(target),
+                    utils.YELLOW
+                )
+                answer = utils.user_input("Do you confirm? (Y/n) ")
+                if answer.lower().startswith("n"):
+                    return
+            shutil.rmtree(target)
+
+        prefix = ". {}; ".format(
+            os.path.join(self.venv_path, "bin", "activate"))
+        args = [
+            "--collectstatic",
+            "--timezone", self.config.get("kalabash", "timezone"),
+            "--domain", self.config.get("general", "hostname"),
+            "--extensions", " ".join(self.extensions),
+            "--dont-install-extensions",
+            "--dburl", "'default:{}://{}:{}@{}/{}'".format(
+                self.config.get("database", "engine"),
+                self.dbuser, self.dbpasswd, self.dbhost, self.dbname
+            )
+        ]
+        if self.devmode:
+            args = ["--devel"] + args
+        if self.amavis_enabled:
+            args += [
+                "'amavis:{}://{}:{}@{}/{}'".format(
+                    self.config.get("database", "engine"),
+                    self.config.get("amavis", "dbuser"),
+                    self.config.get("amavis", "dbpassword"),
+                    self.dbhost,
+                    self.config.get("amavis", "dbname")
+                )
+            ]
+        code, output = utils.exec_cmd(
+            "bash -c '{} kalabash-admin.py deploy instance {}'".format(
+                prefix, " ".join(args)),
+            sudo_user=self.user, cwd=self.home_dir)
+        if code:
+            raise utils.FatalError(output)
+
+    def setup_database(self):
+        """Additional config."""
+        super(Kalabash, self).setup_database()
+        if not self.amavis_enabled:
+            return
+        self.backend.grant_access(
+            self.config.get("amavis", "dbname"), self.dbuser)
+
+    def get_packages(self):
+        """Include extra packages if needed."""
+        packages = super(Kalabash, self).get_packages()
+        condition = (
+            package.backend.FORMAT == "rpm" and
+            sys.version_info.major == 2 and
+            sys.version_info.micro < 9)
+        if condition:
+            # Add extra packages to fix the SNI issue
+            packages += ["openssl-devel"]
+        return packages
+
+    def get_config_files(self):
+        """Return appropriate path."""
+        config_files = super().get_config_files()
+        if package.backend.FORMAT == "deb":
+            path = "supervisor=/etc/supervisor/conf.d/policyd.conf"
+        else:
+            path = "supervisor=/etc/supervisord.d/policyd.ini"
+        config_files.append(path)
+        return config_files
+
+    def get_template_context(self):
+        """Additional variables."""
+        context = super(Kalabash, self).get_template_context()
+        extensions = self.config.get("kalabash", "extensions")
+        extensions = extensions.split()
+        context.update({
+            "sudo_user": (
+                "uwsgi" if package.backend.FORMAT == "rpm" else context["user"]
+            ),
+            "dovecot_mailboxes_owner": (
+                self.config.get("dovecot", "mailboxes_owner")),
+            "radicale_enabled": (
+                "" if "kalabash-radicale" in extensions else "#"),
+            "opendkim_user": self.config.get("opendkim", "user"),
+        })
+        return context
+
+    def apply_settings(self):
+        """Configure kalabash."""
+        rrd_root_dir = os.path.join(self.home_dir, "rrdfiles")
+        pdf_storage_dir = os.path.join(self.home_dir, "pdfcredentials")
+        webmail_media_dir = os.path.join(
+            self.instance_path, "media", "webmail")
+        pw = pwd.getpwnam(self.user)
+        for d in [rrd_root_dir, pdf_storage_dir, webmail_media_dir]:
+            utils.mkdir(d, stat.S_IRWXU | stat.S_IRWXG, pw[2], pw[3])
+        settings = {
+            "admin": {
+                "handle_mailboxes": True,
+                "account_auto_removal": True
+            },
+            "kalabash_amavis": {
+                "am_pdp_mode": "inet",
+            },
+            "maillog": {
+                "rrd_rootdir": rrd_root_dir,
+            },
+            "kalabash_pdfcredentials": {
+                "storage_dir": pdf_storage_dir
+            },
+            "kalabash_radicale": {
+                "server_location": "https://{}/radicale/".format(
+                    self.config.get("general", "hostname")),
+                "rights_file_path": "{}/rights".format(
+                    self.config.get("radicale", "config_dir"))
+            }
+        }
+        for path in ["/var/log/maillog", "/var/log/mail.log"]:
+            if os.path.exists(path):
+                settings["maillog"]["logfile"] = path
+        if self.config.getboolean("opendkim", "enabled"):
+            settings["admin"]["dkim_keys_storage_dir"] = (
+                self.config.get("opendkim", "keys_storage_dir"))
+        settings = json.dumps(settings)
+        query = (
+            "UPDATE core_localconfig SET _parameters='{}'"
+            .format(settings)
+        )
+        self.backend._exec_query(
+            query, self.dbname, self.dbuser, self.dbpasswd)
+
+    def post_run(self):
+        """Additional tasks."""
+        self._setup_venv()
+        self._deploy_instance()
+        if not self.upgrade:
+            self.apply_settings()
+
+        if 'centos' in utils.dist_name():
+            supervisor = "supervisord"
+            system.enable_and_start_service("redis")
+        else:
+            supervisor = "supervisor"
+        # Restart supervisor
+        system.enable_service(supervisor)
+        utils.exec_cmd("service {} stop".format(supervisor))
+        utils.exec_cmd("service {} start".format(supervisor))
